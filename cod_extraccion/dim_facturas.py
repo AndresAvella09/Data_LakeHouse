7c8bfac008d3db5bf094d1e88aacaf54aec42b8f
AndresAvella09 <diegoandresavella@gmail.com>
2025-09-29 09:23:59 -0500

    First Version

---
+++ cod_extraccion/dim_facturas.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+# make sure to install these packages before running:
+# pip install pandas
+# pip install sodapy
+
+import pandas as pd
+from sodapy import Socrata
+
+# Unauthenticated client only works with public data sets. Note 'None'
+# in place of application token, and no username or password:
+client = Socrata("www.datos.gov.co", None)
+
+# Example authenticated client (needed for non-public datasets):
+# client = Socrata(www.datos.gov.co,
+#                  MyAppToken,
+#                  username="user@example.com",
+#                  password="AFakePassword")
+
+
+query = """
+SELECT DISTINCT valor_del_contrato, valor_de_pago_adelantado, valor_facturado, valor_facturado, valor_pagado, 
+"""
+
+results = client.get("jbjy-vk9h", query=query)
+
+# Convert to pandas DataFrame
+results_df = pd.DataFrame.from_records(results)
